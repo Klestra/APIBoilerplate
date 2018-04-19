cf5ab257bb15f982cd9efccda86b3255de2ce72a
admin <admin@iMac3-10.local>
2018-04-19 08:54:14 +0200

    test API fonctionnality

--- script/main.js
+++ script/main.js
@@ -1,25 +1,43 @@
-  //All the JS and JQuery code will be written inside this anonymous function
-$(document).ready(function() {
-    // This function launch our application, and is tasked with the initialisation of every aspect of it
+  //All the JS and JQuery code will de written inside this anonymous function
+  $(document).ready(function () {
+
+    //This function launch our application, and is tasked with the initialisation of every aspect of it
     function init(showAtStartup) {
         navigateView(showAtStartup);
-        $('[data-target]').on('click', function() {
+        $('[data-target]').on('click', function () {
             navigateView(this.dataset.target);
         });
-        $('[data-ajax]').on('click', function() {
-            //https://swapi.co/api/people/1
-            $.get("https://swapi.co/api/people/1", function(data) {
-                let name = "Nom et prénom : " + data.name;
-                let birthDate = "Date de naissance : " + data.birth_year;
-                let mass = "Poid : " + data.mass;
-                $('[data-page="home"]').append('<p>' + name + '</p>')
-                $('[data-page="home"]').append('<p>' + birthDate + '</p>')
-                $('[data-page="home"]').append('<p>' + mass + '</p>')
+        $('[data-ajax]').on('click', function () {
+            $.get("https://dog.ceo/api/breeds/list/all", function (data) {
+                let breedsList = Object.keys(data.message);
+                let list = document.createElement('ul');
+                list.classList.add('ul-no-style');
+                for (let i = 0; i < breedsList.length; i++) {
+                    let listElement = document.createElement('li');
+                    listElement.innerHTML = breedsList[i];
+                    list.appendChild(listElement);
+                };
+                document.querySelector('[data-page="home"]').appendChild(list);
+
+                $('li').on('click', function() {
+                    $.get("https://dog.ceo/api/breed/" + this.innerHTML + "/images/random", function (imgs) {
+                        if($('.modal').length > 0) {
+                            $('.modal').remove();
+                        };
+                        let imgDiv = document.createElement('div');
+                        imgDiv.classList.add('modal');
+                        let img = document.createElement('img');
+                        img.src = imgs.message;
+                        imgDiv.appendChild(img);
+                        document.querySelector('body').appendChild(imgDiv);
+                    });
+                });
             });
         });
-    }
-    // Allow to hide and show every section of our application
-    function navigateView(sectionToShow) {
+    };
+
+    //Allow to hide and show every section of our application
+    function navigateView (sectionToShow) {
         let sections = $('[data-page]');
         let error = true;
         for (let i = 0; i < sections.length; i++) {
@@ -29,11 +47,10 @@ $(document).ready(function() {
             } else {
                 sections[i].classList.add('hidden');
             }
-       };
-       if(error) {
-           throw ('Selector error, nothing found for ' + sectionToShow);
-       }
-        //$('[data-page="' + sectionToShow + '"]').removeClass('hidden');
+        }
+        if(error) {
+            throw ('Selector error, nothing found for ' + sectionToShow);
+        }
     }
     init('home');
 });
